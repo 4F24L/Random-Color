23e8c1cf88a86af57a0b4ab37b23e617f2b90445
4F24L <108697331+4F24L@users.noreply.github.com>
2024-03-26 11:15:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function ranColor(){
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+
+    let bgRandom = "rgb("+x+", "+y+", "+z+")";
+    
+    let bgColor = document.querySelector(".wrapper");
+    bgColor.style.backgroundColor = bgRandom;
+
+    document.querySelector(".color").innerHTML = bgRandom;
+
+}
+
+
+
